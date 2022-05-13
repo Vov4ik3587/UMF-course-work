c6d133887dea53f8b4dbbc611e0ca5b351736ce6
Vov4ik3587 <galstyan.vova@gmail.com>
2022-05-13 17:09:08 +0700

    чета сделал

--- src/All_Math.cs
+++ src/All_Math.cs
@@ -2,6 +2,58 @@ namespace kursach
 {
     public class All_Math
     {
+        /// <summary>
+        /// Matrix multiplication by vector
+        /// </summary>
+        /// <param name="m"> Matrix, stored in 2-dim double array</param>
+        /// <param name="b"> Vector, that multiplies matrix </param>
+        /// <returns> Resulting vector of multiplication </returns>
+        public static double[] MatMul(double[,] m, double[] b)
+        {
+            var columns = m.GetLength(0);
+            var rows = m.GetLength(1);
+
+            var res = new double[rows];
+
+            for (var i = 0; i < rows; i++)
+            {
+                for (var j = 0; j < columns; j++)
+                {
+                    res[i] += m[i, j] * b[j];
+                }
+            }
+
+            return res;
+        }
+
+        /// <summary>
+        /// Matrix multiplication by vector
+        /// </summary>
+        /// <param name="m"> Matrix, stored in sparse format</param>
+        /// <param name="b"> Vector, that multiplies matrix </param>
+        /// <returns> Resulting vector of multiplication </returns>
+        public static double[] MatMul(Matrix m, double[] b)
+        {
+            if (m.Size != b.Length)
+            {
+                throw new Exception($"[ERR] Different sizes. Matrix size = {m.Size}, vector size = {b.Length}");
+            }
+
+            var res = new double[b.Length];
+
+            for (var i = 0; i < b.Length; i++)
+            {
+                res[i] = m.Di[i] * b[i];
+
+                for (var j = m.Ig[i]; j < m.Ig[i + 1]; j++)
+                {
+                    res[i] += m.Ggl[j] * b[m.Jg[j]];
+                    res[m.Jg[j]] += m.Ggu[j] * b[i];
+                }
+            }
+
+            return res;
+        }
         // Добавить Сложение, произведение матриц в разреженном формате
 
         // TODO: Страница кирпича 217 
--- src/Matrix.cs
+++ src/Matrix.cs
@@ -1,13 +1,129 @@
-﻿namespace kursach;
-
+﻿using static System.Math;
+    
+namespace kursach;
 
 /// <summary>
 /// Матрица в разреженно-строчном формате
 /// </summary>
 public class Matrix
 {
-    public double[] Aelem { get; }
-    public double[] Jptr { get; } // Номера столбцов соответствующих ненулевых элементов
-    public double[] Ia { get; } 
-    
+    /// <summary>
+    ///     al is for elements of lower triangular part of matrix
+    /// </summary>
+    /// <returns></returns>
+    public double[] Ggl;
+
+    /// <summary>
+    ///     au is for elements of upper triangular part of matrix
+    /// </summary>
+    public double[] Ggu;
+
+    /// di is for diagonal elements
+    public readonly double[] Di;
+
+    /// <summary>
+    ///     ia is for profile matrix. i.e. by manipulating ia elements we can use our matrix
+    ///     much more time and memory efficient
+    /// </summary>
+    public int[] Ig;
+
+    /// <summary>
+    ///     Ja is for profile matrix. i.e. by manipulating ia elements we can use our matrix
+    ///     much more time and memory efficient
+    /// </summary>
+    public int[] Jg;
+
+    public Matrix()
+    {
+        Ggl = default!;
+        Ggu = default!;
+        Di = default!;
+        Ig = default!;
+        Jg = default!;
+        Decomposed = default!;
+        Size = default!;
+    }
+
+    public Matrix(double[] ggl, double[] ggu, double[] di, int[] ig, int[] jg, int size, bool decomposed)
+    {
+        Ggl = ggl;
+        Ggu = ggu;
+        Di = di;
+        Ig = ig;
+        Jg = jg;
+        Size = size;
+        Decomposed = decomposed;
+    }
+
+    /// <summary>
+    /// Stores info about LU decomposed or not
+    /// </summary>
+    public bool Decomposed { get; private set; }
+
+    public int Size { get; }
+
+    /// <summary>
+    /// LU(sq)-decomposition with value=1 in diagonal elements of U matrix.
+    /// Corrupts base object. To access data as one matrix you need to build it from L and U.
+    /// </summary>
+    /// <exception cref="DivideByZeroException"> If diagonal element is zero </exception>
+    public void Factorize()
+    {
+        for (var i = 0; i < Size; i++)
+        {
+            var sumDi = 0.0;
+
+            var i0 = Ig[i];
+            var i1 = Ig[i + 1];
+
+            for (var k = i0; k < i1; k++)
+            {
+                var j = Jg[k];
+                var j0 = Ig[j];
+                var j1 = Ig[j + 1];
+
+                var iK = i0;
+                var kJ = j0;
+
+                var sumL = 0.0;
+                var sumU = 0.0;
+
+                while (iK < k && kJ < j1)
+                {
+                    if (Jg[iK] == Jg[kJ])
+                    {
+                        sumL += Ggl[iK] * Ggu[kJ];
+                        sumU += Ggu[iK] * Ggl[kJ];
+                        iK++;
+                        kJ++;
+                    }
+                    else
+                    {
+                        if (Jg[iK] > Jg[kJ])
+                        {
+                            kJ++;
+                        }
+                        else
+                        {
+                            iK++;
+                        }
+                    }
+                }
+
+                if (Di[j] == 0.0)
+                {
+                    throw new DivideByZeroException($"Di[{j}] has thrown at pos {i} {j}");
+                }
+                Ggl[k] = (Ggl[k] - sumL) / Di[j];
+                Ggu[k] = (Ggu[k] - sumU) / Di[j];
+                
+                sumDi += Ggl[k] * Ggu[k];
+            }
+
+            Di[i] = Sqrt(Di[i] - sumDi);
+        }
+
+        Decomposed = true;
+    }
 }
+
